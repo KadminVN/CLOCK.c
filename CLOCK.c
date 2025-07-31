d5754f99e71e3bd1e964d1f737722de16d336b4e
Kadminisbr <90295947+KadminVN@users.noreply.github.com>
2025-07-31 10:19:50 +0700

    V1.5!!!!!!!!!!!!!!!!!!!!!!!!
    - Fixed everything?

--- CLOCK.c
+++ CLOCK.c
@@ -1,5 +1,6 @@
 /*--------------------------
     CLOCK.C -- Analog Clock Program
+    (Improved version with embedded resources)
 ---------------------------*/
 
 #include <windows.h>
@@ -19,6 +20,12 @@
 #define ID_DOTS_BTN 6
 #define ID_MYSTERY_BTN 7
 #define TWOPI (2*3.14159)
+
+// Resource IDs (must match your .rc file)
+#define IDI_SOUND_ON     101
+#define IDI_SOUND_OFF    102
+#define IDR_MC_FONT      103
+#define IDR_PIXEL_FONT   104
 
 // Global variables
 BOOL g_bDarkMode = FALSE;
@@ -50,26 +57,24 @@ const TCHAR* romanNumerals[] = {
     TEXT("VI"), TEXT("VII"), TEXT("VIII"), TEXT("IX"), TEXT("X"), TEXT("XI"), TEXT("XII")
 };
 
+// Function prototypes
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void DrawColorfulQuestionMarks(HWND hwnd);
+void UpdateButtonFonts(HWND hwnd);
+BOOL LoadEmbeddedResources(HINSTANCE hInstance);
+void FreeResources();
+void PlayMysteryVideo();
 
-// Helper function to update all button fonts
-void UpdateButtonFonts(HWND hwnd)
+// Helper function to get executable directory
+BOOL GetExeDirectory(TCHAR* buffer, DWORD size)
 {
-    HFONT hCurrentBtnFont = g_bUsePixelFont ? hPixelBtnFont : hMinecraftBtnFont;
+    if (!GetModuleFileName(NULL, buffer, size))
+        return FALSE;
     
-    if (hBtnDarkMode) SendMessage(hBtnDarkMode, WM_SETFONT, (WPARAM)hCurrentBtnFont, TRUE);
-    if (hBtnRomanMode) SendMessage(hBtnRomanMode, WM_SETFONT, (WPARAM)hCurrentBtnFont, TRUE);
-    if (hBtnFontSwitch) SendMessage(hBtnFontSwitch, WM_SETFONT, (WPARAM)hCurrentBtnFont, TRUE);
-    if (hBtnDots) SendMessage(hBtnDots, WM_SETFONT, (WPARAM)hCurrentBtnFont, TRUE);
-    if (hBtnMystery) SendMessage(hBtnMystery, WM_SETFONT, (WPARAM)hCurrentBtnFont, TRUE);
+    TCHAR* lastSlash = _tcsrchr(buffer, TEXT('\\'));
+    if (lastSlash)
+        *lastSlash = TEXT('\0');
     
-    // Force redraw of all buttons
-    InvalidateRect(hBtnDarkMode, NULL, TRUE);
-    InvalidateRect(hBtnRomanMode, NULL, TRUE);
-    InvalidateRect(hBtnFontSwitch, NULL, TRUE);
-    InvalidateRect(hBtnDots, NULL, TRUE);
-    InvalidateRect(hBtnMystery, NULL, TRUE);
+    return TRUE;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
@@ -78,6 +83,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
     HWND hwnd;
     MSG msg;
     WNDCLASS wndclass;
+
+    // Load embedded resources
+    if (!LoadEmbeddedResources(hInstance))
+    {
+        MessageBox(NULL, TEXT("Failed to load required resources!"), szAppName, MB_ICONERROR);
+        return 0;
+    }
 
     wndclass.style = CS_HREDRAW | CS_VREDRAW;
     wndclass.lpfnWndProc = WndProc;
@@ -93,14 +105,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
     if (!RegisterClass(&wndclass))
     {
         MessageBox(NULL, TEXT("Program requires Windows NT!"), szAppName, MB_ICONERROR);
+        FreeResources();
         return 0;
     }
 
     hwnd = CreateWindow(szAppName, TEXT("Analog Clock"),
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
-        800, 600,  // Set initial window size
+        800, 600,
         NULL, NULL, hInstance, NULL);
+
+    if (!hwnd)
+    {
+        FreeResources();
+        return 0;
+    }
 
     ShowWindow(hwnd, iCmdShow);
     UpdateWindow(hwnd);
@@ -110,165 +129,122 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    FreeResources();
     return msg.wParam;
 }
 
-void SetIsotropic(HDC hdc, int cxClient, int cyClient)
+// Resource loading functions
+BOOL LoadEmbeddedResources(HINSTANCE hInstance)
 {
-    int iScale = (cxClient < cyClient ? cxClient : cyClient) / 2;
-    if (iScale == 0) iScale = 1;
-
-    SetMapMode(hdc, MM_ISOTROPIC);
-    SetWindowExtEx(hdc, 600, 600, NULL);
-    SetViewportExtEx(hdc, iScale, -iScale, NULL);
-    SetViewportOrgEx(hdc, cxClient / 2, cyClient / 2, NULL);
-}
-
-void RotatePoint(POINT pt[], int iNum, int iAngle)
-{
-    int i;
-    POINT ptTemp;
-
-    for (i = 0; i < iNum; i++)
+    // Load icons from resources
+    hSoundOnIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SOUND_ON));
+    hSoundOffIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SOUND_OFF));
+    
+    if (!hSoundOnIcon || !hSoundOffIcon)
     {
-        ptTemp.x = (int)(pt[i].x * cos(TWOPI * iAngle / 360) +
-            pt[i].y * sin(TWOPI * iAngle / 360));
-        ptTemp.y = (int)(pt[i].y * cos(TWOPI * iAngle / 360) -
-            pt[i].x * sin(TWOPI * iAngle / 360));
-        pt[i] = ptTemp;
+        // Fallback to system icons if embedded ones fail
+        hSoundOnIcon = LoadIcon(NULL, IDI_INFORMATION);
+        hSoundOffIcon = LoadIcon(NULL, IDI_WARNING);
     }
-}
 
-void DrawClock(HDC hdc)
-{
-    int iAngle;
-    POINT pt[3];
-    HBRUSH hBrush, hOldBrush;
-    HFONT hOldFont = NULL;
-    HFONT hCurrentFont = g_bUsePixelFont ? hPixelFont : hMinecraftFont;
-
-    hBrush = CreateSolidBrush(g_bDarkMode ? RGB(255, 255, 255) : RGB(0, 0, 0));
-    hOldBrush = SelectObject(hdc, hBrush);
-
-    if (g_bShowDots)
+    // Add embedded fonts
+    HRSRC hRes = FindResource(hInstance, MAKEINTRESOURCE(IDR_MC_FONT), RT_FONT);
+    if (hRes)
     {
-        for (iAngle = 0; iAngle < 360; iAngle += 6)
+        HGLOBAL hFontRes = LoadResource(hInstance, hRes);
+        if (hFontRes)
         {
-            pt[0].x = 0;
-            pt[0].y = 500;
-
-            RotatePoint(pt, 1, iAngle);
-
-            pt[2].x = pt[2].y = iAngle % 5 ? 18 : 55;
-
-            pt[0].x -= pt[2].x / 2;
-            pt[0].y -= pt[2].y / 2;
-
-            pt[1].x = pt[0].x + pt[2].x;
-            pt[1].y = pt[0].y + pt[2].y;
-
-            Ellipse(hdc, pt[0].x, pt[0].y, pt[1].x, pt[1].y);
+            DWORD nFonts = 0;
+            AddFontMemResourceEx(hFontRes, SizeofResource(hInstance, hRes), NULL, &nFonts);
         }
     }
 
-    for (iAngle = 0; iAngle < 360; iAngle += 30)
+    hRes = FindResource(hInstance, MAKEINTRESOURCE(IDR_PIXEL_FONT), RT_FONT);
+    if (hRes)
     {
-        if (hCurrentFont) {
-            int hour = iAngle / 30;
-            if (hour == 0) hour = 12;
-            double rad = iAngle * 3.14159265358979323846 / 180.0;
-            int numRadius = 450;
-            int tx = (int)(0 + numRadius * sin(rad));
-            int ty = (int)(0 + numRadius * cos(rad)) + 35;
-            const TCHAR* label = g_bRomanMode ? romanNumerals[hour] : NULL;
-            TCHAR buf[8];
-            if (g_bRomanMode) {
-                lstrcpy(buf, label);
-            }
-            else {
-                wsprintf(buf, TEXT("%d"), hour);
-            }
-            hOldFont = (HFONT)SelectObject(hdc, hCurrentFont);
-            SetBkMode(hdc, TRANSPARENT);
-            SetTextColor(hdc, g_bDarkMode ? RGB(255, 255, 255) : RGB(0, 0, 0));
-            SIZE sz;
-            GetTextExtentPoint32(hdc, buf, lstrlen(buf), &sz);
-            TextOut(hdc, tx - sz.cx / 2, ty - sz.cy / 2, buf, lstrlen(buf));
-            SelectObject(hdc, hOldFont);
+        HGLOBAL hFontRes = LoadResource(hInstance, hRes);
+        if (hFontRes)
+        {
+            DWORD nFonts = 0;
+            AddFontMemResourceEx(hFontRes, SizeofResource(hInstance, hRes), NULL, &nFonts);
         }
     }
 
-    SelectObject(hdc, hOldBrush);
-    DeleteObject(hBrush);
-}
+    // Create fonts
+    hMinecraftFont = CreateFont(-40, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
+        DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+        FF_DONTCARE, TEXT("Minecraft"));
 
-void DrawHands(HDC hdc, SYSTEMTIME * pst, BOOL fChange)
-{
-    static POINT pt[3][5] = {
-        {0, -110, 70, 0, 0, 300, -70, 0, 0, -110},
-        {0, -150, 40, 0, 0, 420, -40, 0, 0, -150},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 420}
-    };
-    int i, iAngle[3];
-    POINT ptTemp[3][5];
-    HPEN hPen, hOldPen;
-    COLORREF handColor = g_bDarkMode ? RGB(255, 255, 255) : RGB(0, 0, 0);
+    hMinecraftBtnFont = CreateFont(-24, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
+        DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+        FF_DONTCARE, TEXT("Minecraft"));
 
-    iAngle[0] = (pst->wHour * 30) % 360 + pst->wMinute / 2;
-    iAngle[1] = pst->wMinute * 6;
-    iAngle[2] = pst->wSecond * 6;
+    hPixelFont = CreateFont(-24, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
+        DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+        FF_DONTCARE, TEXT("Dogica"));
 
-    memcpy(ptTemp, pt, sizeof(pt));
+    hPixelBtnFont = CreateFont(-16, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
+        DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+        FF_DONTCARE, TEXT("Dogica"));
 
-    hPen = CreatePen(PS_SOLID, 0, handColor);
-    hOldPen = SelectObject(hdc, hPen);
-
-    for (i = fChange ? 0 : 2; i < 3; i++)
+    // Fallback to system fonts if custom fonts fail
+    if (!hMinecraftFont || !hMinecraftBtnFont)
     {
-        RotatePoint(ptTemp[i], 5, iAngle[i]);
-        Polyline(hdc, ptTemp[i], 5);
+        hMinecraftFont = hMinecraftBtnFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+    }
+    if (!hPixelFont || !hPixelBtnFont)
+    {
+        hPixelFont = hPixelBtnFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
     }
 
-    SelectObject(hdc, hOldPen);
-    DeleteObject(hPen);
+    return TRUE;
 }
 
-void DrawColorfulQuestionMarks(HWND hwnd)
+void FreeResources()
 {
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(hwnd, &ps);
-    
-    // Get button dimensions
-    RECT rc;
-    GetClientRect(hwnd, &rc);
-    
-    // Set up the font
-    HFONT hOldFont = (HFONT)SelectObject(hdc, g_bUsePixelFont ? hPixelBtnFont : hMinecraftBtnFont);
-    
-    // Calculate text position
-    const TCHAR* text = TEXT("???");
-    SIZE sz;
-    GetTextExtentPoint32(hdc, text, 3, &sz);
-    int x = (rc.right - sz.cx) / 2;
-    int y = (rc.bottom - sz.cy) / 2;
-    
-    // Set background mode
-    SetBkMode(hdc, TRANSPARENT);
-    
-    // Draw each question mark with different color
-    for (int i = 0; i < 3; i++)
+    if (hMinecraftFont && hMinecraftFont != (HFONT)GetStockObject(DEFAULT_GUI_FONT))
+        DeleteObject(hMinecraftFont);
+    if (hMinecraftBtnFont && hMinecraftBtnFont != (HFONT)GetStockObject(DEFAULT_GUI_FONT))
+        DeleteObject(hMinecraftBtnFont);
+    if (hPixelFont && hPixelFont != (HFONT)GetStockObject(DEFAULT_GUI_FONT))
+        DeleteObject(hPixelFont);
+    if (hPixelBtnFont && hPixelBtnFont != (HFONT)GetStockObject(DEFAULT_GUI_FONT))
+        DeleteObject(hPixelBtnFont);
+
+    if (hSoundOnIcon && hSoundOnIcon != LoadIcon(NULL, IDI_INFORMATION))
+        DestroyIcon(hSoundOnIcon);
+    if (hSoundOffIcon && hSoundOffIcon != LoadIcon(NULL, IDI_WARNING))
+        DestroyIcon(hSoundOffIcon);
+}
+
+void PlayMysteryVideo()
+{
+    TCHAR exePath[MAX_PATH];
+    if (GetExeDirectory(exePath, MAX_PATH))
     {
-        COLORREF colors[] = {RGB(255, 0, 0), RGB(0, 255, 0), RGB(0, 0, 255)}; // Red, Green, Blue
-        SetTextColor(hdc, colors[i]);
+        TCHAR videoPath[MAX_PATH];
+        _stprintf(videoPath, TEXT("%s\\ASTELLION.mp4"), exePath);
         
-        // Draw single character
-        TCHAR ch[2] = {text[i], 0};
-        TextOut(hdc, x + i * (sz.cx / 3), y, ch, 1);
+        // Try to play the video
+        HINSTANCE result = ShellExecute(NULL, TEXT("open"), videoPath, NULL, NULL, SW_SHOWNORMAL);
+        
+        // If failed, show a message
+        if ((INT_PTR)result <= 32)
+        {
+            MessageBox(NULL, 
+                TEXT("Could not play the mystery video!\n")
+                TEXT("Please make sure ASTELLION.mp4 is in the same folder as this program."),
+                TEXT("Clock"), MB_ICONINFORMATION);
+        }
     }
-    
-    // Clean up
-    SelectObject(hdc, hOldFont);
-    EndPaint(hwnd, &ps);
+    else
+    {
+        MessageBox(NULL, 
+            TEXT("Could not locate the program directory!\n")
+            TEXT("Please make sure ASTELLION.mp4 is in the same folder as this program."),
+            TEXT("Clock"), MB_ICONINFORMATION);
+    }
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -511,9 +487,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             if (LOWORD(wParam) == ID_MYSTERY_BTN)
             {
-                // Play the video using the full path
-                ShellExecute(NULL, TEXT("open"), TEXT("d:\\Git Uploads\\CLOCK.c\\ASTELLION.mp4"), 
-                            NULL, NULL, SW_SHOWNORMAL);
+                PlayMysteryVideo();
                 return 0;
             }
             break;
---
+++ resource.h
@@ -0,0 +1,4 @@
+#define IDI_SOUND_ON     101
+#define IDI_SOUND_OFF    102
+#define IDR_MC_FONT      103
+#define IDR_PIXEL_FONT   104
